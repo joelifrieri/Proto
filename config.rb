246dce22c85c8a8fcea4c919ddb3d209222d914c
Joe Lifrieri <joe@hugsformonsters.com>
2012-06-20 15:37:47 -0400

    Initial push

---
+++ config.rb
@@ -0,0 +1,10 @@
+http_path = '/'
+css_dir = 'css'
+sass_dir = 'css/src'
+images_dir = 'img'
+http_images_path = 'img'
+# probably don't want to edit beneath this line unless you really know what you want to do
+relative_assets = true
+output_style = :compressed
+preferred_syntax = :scss
+asset_cache_buster :none
